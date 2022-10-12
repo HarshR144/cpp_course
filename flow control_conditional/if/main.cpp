3ec84637ba22ee3628d04aa99c4fab3cc2de35ae
HarshRepal <harsh.repal2003@gamil.com>
2022-10-12 17:33:22 +0530

    commiting changes day 2

--- flow control_conditional/if/main.cpp
+++ flow control_conditional/if/main.cpp
@@ -5,16 +5,36 @@ int main() {
 
 int no1 {10};
 int no2 {20};
-bool result {no1 > no2};
-cout<<boolalpha;
+bool result =(no1 > no2);
+cout<<boolalpha<<result<<endl;
 
 
 if (result == true){
-    cout<<"Yes no1 is greater than no 2"<<endl;
+cout<<"Yes no1 is greater than no 2"<<endl;
 }
-if (result != false) {
-    cout<<"NO, no1 is smaller"<<endl;
+/*if(!(result == true)) {      //if not result is true
+cout<<"NO, no1 is smaller"<<endl;
+}*/
+else {
+cout<<"NO, no1 is smaller"<<endl;
+
+cout<<endl;
+cout<<"------------------------------------";
+
+bool light_green =true;
+bool police_stops =true;
+
+cout<<"police"<<endl;
+if (light_green){
+    cout<<"lets go"<<endl;
+    if(police_stops){
+        cout<<"stop"<<endl;
+    }
 }
 
-return 0;
+
+
+
+
+
 }
